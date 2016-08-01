291e6938dd49ba62f227ffc060ef6883dfe0a9e6
Thomas Carli <thomas@procore.com>
2016-08-01 06:56:08 +0000

    add additional images and bottom container

--- extension_pack/set_image.js
+++ extension_pack/set_image.js
@@ -2,6 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
   set_body_styles();
 
   var image_container = document.getElementById("background-image");
+  var bottom_container = document.getElementsByClassName("bottom-container")[0];
+  var name_container = document.getElementsByClassName("name-container")[0];
+  var episode_info_container = document.getElementsByClassName("episode-info-container")[0];
+
+  set_name_container(name_container);
+  set_episode_info_container(episode_info_container);
+  set_bottom_container_styles(bottom_container);
   set_image_container_styles(image_container);
 });
 
@@ -21,11 +28,31 @@ function set_image_container_styles(image_container) {
   image_container.style.backgroundSize = "cover";
 }
 
+function set_name_container(name_container) {
+  name_container.style.fontSize = "15px";
+  name_container.style.color = "whitesmoke";
+  name_container.style.paddingRight = "200px";
+}
+
+function set_episode_info_container(episode_info_container) {
+  episode_info_container.style.fontSize = "15px";
+  episode_info_container.style.color = "whitesmoke";
+  episode_info_container.style.paddingLeft = "200px";
+}
+
+function set_bottom_container_styles(bottom_container) {
+  bottom_container.style.display = "flex";
+  bottom_container.style.alignItems = "baseline";
+  bottom_container.style.position = "absolute";
+  bottom_container.style.bottom = "0";
+  bottom_container.style.width = "100%";
+  bottom_container.style.justifyContent = "space-between";
+}
+
 function randomize_image() {
   var base_string = "url('images/friends_";
-  var random_int = getRandomInt(1,2).toString();
+  var random_int = getRandomInt(1,9).toString();
   var result = base_string.concat(random_int).concat(".jpg')");
-  console.log(result);
   return result;
 }
 
